d5d2d5ad655abfbece6bc9d09a7c668fbb64650f
Eduardo Farias <edo_farias@icloud.com>
2016-09-15 14:18:52 -0300

    Initial Commit

---
+++ MiniReto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniReto
+//
+//  Created by Eduardo Farías on 15-09-16.
+//  Copyright © 2016 Eduardo Farías. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
